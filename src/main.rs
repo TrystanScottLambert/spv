792de9bea6298a30a4510c43fd7270f16864c059
TrystanScottLambert <trystanscottlambert@gmail.com>
2026-01-16 11:53:56 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use chrono::prelude::*;
+use colored::Colorize;
+
+fn main() {
+    println!("{}", "Survey Program Validation".bold());
+    let start_date = Utc.with_ymd_and_hms(2026, 3, 6, 12, 0, 0).unwrap();
+    let today_date = Utc::now();
+    let elapsed = start_date.signed_duration_since(today_date);
+    println!(
+        "There are about {} weeks until SPV.",
+        elapsed.num_weeks().to_string().bold().red()
+    );
+}
